de4f2449ad2c54dfe4e82aa959304c4293636e16
unknown <>
2017-03-04 11:43:46 +0800

    add nextTick to refresh

--- src/jroll-scroller.vue
+++ src/jroll-scroller.vue
@@ -44,23 +44,23 @@
     methods: {
       // 当scroller或wrapper的高度发生变化时，需要用此方法对JRoll对象进行刷新
       refresh () {
-        return this.jroll.refresh();
+        this.$nextTick(() => this.jroll.refresh());
       },
       // 该方法用于移动scroller，共五个参数，第一个参数是x偏移量（必填），第二个是y偏移量（必填），第三个是滑动时间（可选，单位ms)，第四个是是否允许超出边界（可选，true/false），第五个回调方法（可选）。如果想获取当前x,y偏移量，可直接输出jroll.x和jroll.y
       scrollTo (...args) {
-        return this.jroll.scrollTo.apply(this.jroll, args);
+        this.jroll.scrollTo.apply(this.jroll, args);
       },
       // 使能滑动，使用disable禁止滑动后可用该方法重新开启
       enable () {
-        return this.jroll.enable();
+        this.jroll.enable();
       },
       // 使不能滑动
       disable () {
-        return this.jroll.disable();
+        this.jroll.disable();
       },
       // 缩放，只接受一个整型/浮点型参数
       scale (level) {
-        return this.jroll.scale(level);
+        this.jroll.scale(level);
       }
     }
   };
